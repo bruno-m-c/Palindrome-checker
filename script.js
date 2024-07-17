1c4e49af109abb85f97052f0776790114fbb05ad
bruno-m-c <122916882+bruno-m-c@users.noreply.github.com>
2024-07-17 17:28:05 +0000

    Palindrome checker function completed. Review code to make sure no bug is present.

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const palindromeInput = document.getElementById('inputBar');
 const output = document.getElementById('result');
 
 function checkPalindrome() {
-  const cleanPalindromeInput = palindromeInput.replace(/[+-.:;"',?*/><\s]/gi, "").toLowerCase();
-  let result = `<strong>${palindromeInput}<strong/> ${cleanPalindromeInput === [...cleanPalindromeInput].reverse().join("") ? `is` : `is not`} a palindrome `;
+  const cleanPalindromeInput = palindromeInput.value.replace(/[!+-.:;"',?*/><\s]/gi, "").toLowerCase();
+  let result = `<strong>${palindromeInput.value}</strong> ${cleanPalindromeInput === [...cleanPalindromeInput].reverse().join("") ? `is` : `is not`} a palindrome `;
   output.style.display = "block";
-  output.innerText = result;
+  output.innerHTML = result;
 }
 
 check.addEventListener("click", checkPalindrome);
