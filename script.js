2274b266c5d3670bce7953764032f36b1164c3be
bruno-m-c <122916882+bruno-m-c@users.noreply.github.com>
2024-07-21 23:41:40 +0000

    Added event listener for Enter, empty input bar, adding ground shadow.

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ const check = document.getElementById('checkButton');
 const palindromeInput = document.getElementById('inputBar');
 const output = document.getElementById('result');
 
+
 function checkPalindrome() {
+  if (palindromeInput.value === '') {
+    alert('Please input a value');
+    return;
+  }
   const cleanPalindromeInput = palindromeInput.value.replace(/[!+-.:;"',?*/><\s]/gi, "").toLowerCase();
   let result = `<strong>${palindromeInput.value}</strong> ${cleanPalindromeInput === [...cleanPalindromeInput].reverse().join("") ? `is` : `is not`} a palindrome `;
   output.style.display = "block";
   output.innerHTML = result;
+  palindromeInput.value = ''
 }
 
 check.addEventListener("click", checkPalindrome);
+palindromeInput.addEventListener("keydown", (e) => {
+  if(e.key === "Enter") {
+    e.preventDefault();
+    checkPalindrome();
+  }
+});
