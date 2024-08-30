bc3f038279e20e9e81d29cdffcd9dae6a207caf1
Your Name <you@example.com>
2024-08-30 18:16:30 -0500

    fixing script

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const check = document.getElementById('checkButton');
 const palindromeInput = document.getElementById('inputBar');
 const output = document.getElementById('result');
 const bubble1 = document.getElementById('bubble');
-<<<<<<< HEAD
 const cont5 = document.getElementsByClassName("CONT5")[0];
-=======
 const cont5 = document.getElementsByClassName("CONT5");
->>>>>>> 3e6e15051c54878c1d6cef689315623de853edb8
 
 function checkPalindrome() {
   if (palindromeInput.value === '') {
@@ -20,11 +17,8 @@ function checkPalindrome() {
   palindromeInput.value = '';
   bubble1.classList.remove('hide');
   cont5.classList.remove('hide');
-<<<<<<< HEAD
   cont5.classList.add('move');
-=======
   cont5.classList.add('Transition');
->>>>>>> 3e6e15051c54878c1d6cef689315623de853edb8
 }
 
 check.addEventListener("click", checkPalindrome);
