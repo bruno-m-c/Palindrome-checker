dfb0092c238381e76ed90d0055a1187683aa5583
Your Name <you@example.com>
2024-08-30 19:15:43 -0500

    add functionality to js. is palindrome activates cont5. is not activates bubble1

--- script.js
+++ script.js
@@ -5,17 +5,26 @@ const bubble1 = document.getElementById('bubble');
 const cont5 = document.getElementsByClassName("CONT5")[0];
 
 function checkPalindrome() {
+  cont5.classList.remove('move');
+  bubble1.classList.add('hide');
+
   if (palindromeInput.value === '') {
     alert('Please input a value');
     return;
   }
   const cleanPalindromeInput = palindromeInput.value.replace(/[^A-Za-z0-9]/gi, "").toLowerCase();
-  let result = `<strong>${palindromeInput.value}</strong> ${cleanPalindromeInput === [...cleanPalindromeInput].reverse().join("") ? `is` : `is not`} a palindrome `;
+  const isPalindrome = cleanPalindromeInput === [...cleanPalindromeInput].reverse().join("");
+  let result;
+  if(isPalindrome){
+    result = `<strong>${palindromeInput.value}</strong> is a palindrome `;
+    cont5.classList.add('move');
+  } else {
+    result = `<strong>${palindromeInput.value}</strong> is not a palindrome `;
+    bubble1.classList.remove('hide');
+  }
   output.style.display = "block";
   output.innerHTML = result;
   palindromeInput.value = '';
-  bubble1.classList.remove('hide');
-  cont5.classList.add('move');
 }
 
 check.addEventListener("click", checkPalindrome);
