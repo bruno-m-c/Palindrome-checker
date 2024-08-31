d9225905c989c3e7478d11308eef519c7d2b8edf
Your Name <you@example.com>
2024-08-30 19:07:09 -0500

    fixed js issue.

--- script.js
+++ script.js
@@ -3,22 +3,19 @@ const palindromeInput = document.getElementById('inputBar');
 const output = document.getElementById('result');
 const bubble1 = document.getElementById('bubble');
 const cont5 = document.getElementsByClassName("CONT5")[0];
-const cont5 = document.getElementsByClassName("CONT5");
 
 function checkPalindrome() {
   if (palindromeInput.value === '') {
     alert('Please input a value');
     return;
   }
-  const cleanPalindromeInput = palindromeInput.value.replace(/[!+-_.:();"',?*/><\s]/gi, "").toLowerCase();
+  const cleanPalindromeInput = palindromeInput.value.replace(/[^A-Za-z0-9]/gi, "").toLowerCase();
   let result = `<strong>${palindromeInput.value}</strong> ${cleanPalindromeInput === [...cleanPalindromeInput].reverse().join("") ? `is` : `is not`} a palindrome `;
   output.style.display = "block";
   output.innerHTML = result;
   palindromeInput.value = '';
   bubble1.classList.remove('hide');
-  cont5.classList.remove('hide');
   cont5.classList.add('move');
-  cont5.classList.add('Transition');
 }
 
 check.addEventListener("click", checkPalindrome);
